e399a4f56ef95d8941edc26b4712b4f52851ca09
stepkamipt <stepkamipt@gmail.com>
2025-03-23 19:21:32 +0100

    iter2: add agent metrics monitor

---
+++ cmd/agent/internal/monitor/monitor_test.go
@@ -0,0 +1,19 @@
+package monitor
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetRuntimeGauges(t *testing.T) {
+	_, err := GetRuntimeGauges()
+	require.NoError(t, err)
+}
+
+func TestGetMetrics(t *testing.T) {
+	metrics, err := GetMetrics()
+	require.NoError(t, err)
+	require.Equal(t, 28, len(metrics.Gauges))
+	require.Equal(t, 1, len(metrics.Counters))
+}
