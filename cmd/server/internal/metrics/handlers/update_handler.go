e1226ca2409269b7c1ae27a9a7d6647f62ce3a88
stepkamipt <stepkamipt@gmail.com>
2025-03-24 12:29:43 +0100

    iter3: change server's http server from default to chi

--- cmd/server/internal/metrics/handlers/update_handler.go
+++ cmd/server/internal/metrics/handlers/update_handler.go
@@ -3,91 +3,80 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/stepkareserva/obsermon/cmd/server/internal/metrics/server"
 	"github.com/stepkareserva/obsermon/internal/models"
 )
 
-type UpdateHandler struct {
-	server *server.Server
-}
+const (
+	// metrics for url
+	MetricGauge   = "gauge"
+	MetricCounter = "counter"
 
-func NewUpdateHandler(s *server.Server) (*UpdateHandler, error) {
+	// names of chi routing url params to be extracted
+	ChiMetric = "metric"
+	ChiName   = "name"
+	ChiValue  = "value"
+)
+
+func UpdateHandler(s *server.Server) (http.Handler, error) {
 	if s == nil {
 		return nil, fmt.Errorf("metrics server is nil")
 	}
 
-	handler := UpdateHandler{
-		server: s,
-	}
+	r := chi.NewRouter()
 
-	return &handler, nil
+	r.Post(fmt.Sprintf("/%s/{%s}/{%s}", MetricGauge, ChiName, ChiValue),
+		updateGaugeHandler(s))
+	r.Post(fmt.Sprintf("/%s/{%s}/{%s}", MetricCounter, ChiName, ChiValue),
+		updateCounterHandler(s))
+	r.Post(fmt.Sprintf("/{%s}/{%s}/{%s}", ChiMetric, ChiName, ChiValue),
+		updateUnknownMetricHandler())
+
+	return r, nil
 }
 
-func (h *UpdateHandler) UpdateGaugeHandler(w http.ResponseWriter, r *http.Request) {
-	if err := h.checkValidity(); err != nil {
-		WriteError(w, ErrInternalServerError)
-		return
-	}
+func updateGaugeHandler(s *server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, ChiName)
+		var value models.Gauge
+		if err := value.FromString(chi.URLParam(r, ChiValue)); err != nil {
+			WriteError(w, ErrInvalidMetricValue)
+			return
+		}
 
-	var gauge models.UpdateGaugeRequest
-	if err := gauge.FromURLPath(strings.TrimPrefix(r.URL.Path, "/")); err != nil {
-		WriteError(w, translateURLParsingError(err))
-		return
-	}
+		if err := s.UpdateGauge(name, value); err != nil {
+			WriteError(w, ErrInternalServerError)
+			return
+		}
 
-	if err := h.server.UpdateGauge(gauge.Name, gauge.Value); err != nil {
-		WriteError(w, ErrInternalServerError)
-		return
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 	}
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
 }
 
-func (h *UpdateHandler) UpdateCounterHandler(w http.ResponseWriter, r *http.Request) {
-	if err := h.checkValidity(); err != nil {
-		WriteError(w, ErrInternalServerError)
-		return
-	}
-	var counter models.UpdateCounterRequest
-	if err := counter.FromURLPath(strings.TrimPrefix(r.URL.Path, "/")); err != nil {
-		WriteError(w, translateURLParsingError(err))
-		return
-	}
+func updateCounterHandler(s *server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, ChiName)
+		var value models.Counter
+		if err := value.FromString(chi.URLParam(r, ChiValue)); err != nil {
+			WriteError(w, ErrInvalidMetricValue)
+			return
+		}
 
-	if err := h.server.UpdateCounter(counter.Name, counter.Value); err != nil {
-		WriteError(w, ErrInternalServerError)
-		return
-	}
+		if err := s.UpdateCounter(name, value); err != nil {
+			WriteError(w, ErrInternalServerError)
+			return
+		}
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+	}
 }
 
-func (h *UpdateHandler) UpdateHandler(w http.ResponseWriter, r *http.Request) {
-	if err := h.checkValidity(); err != nil {
-		WriteError(w, ErrInternalServerError)
-		return
-	}
-	WriteError(w, ErrInvalidMetricType)
-}
-
-func (h *UpdateHandler) checkValidity() error {
-	if h == nil || h.server == nil {
-		return fmt.Errorf("server not exists")
-	}
-	return nil
-}
-
-func translateURLParsingError(err error) Error {
-	switch err.(type) {
-	case *models.MissingMetricNameError:
-		return ErrMissingMetricName
-	case *models.InvalidMetricValueError:
-		return ErrInvalidMetricValue
-	default:
-		return ErrInternalServerError
+func updateUnknownMetricHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		WriteError(w, ErrInvalidMetricType, chi.URLParam(r, ChiMetric))
 	}
 }
