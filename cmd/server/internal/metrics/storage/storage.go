196667416617e0558650fec7974c252649966c71
stepkamipt <stepkamipt@gmail.com>
2025-03-27 22:07:42 +0100

    review: rename Error->HandlerError to avoid intersections with standard library's types

--- cmd/server/internal/metrics/storage/storage.go
+++ cmd/server/internal/metrics/storage/storage.go
@@ -2,12 +2,19 @@ package storage
 
 import "github.com/stepkareserva/obsermon/internal/models"
 
-type Storage interface {
+type GaugeStorage interface {
 	SetGauge(val models.Gauge) error
 	GetGauge(name string) (*models.Gauge, bool, error)
 	ListGauges() (models.GaugesList, error)
+}
 
+type CounterStorage interface {
 	SetCounter(val models.Counter) error
 	GetCounter(name string) (*models.Counter, bool, error)
 	ListCounters() (models.CountersList, error)
+}
+
+type Storage interface {
+	GaugeStorage
+	CounterStorage
 }
